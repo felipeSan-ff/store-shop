ea8b7ea085f6ba3d7b9aadd35c43c29b0183d5d4
Felipe Santana <l.felipesantana.ff@gmail.com>
2026-01-07 05:56:08 -0300

    validacao de e-mail e password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  email: string = '';
+  password: string = '';
+  resultOnClickButton: string = '';
+  errorMessage: string = '';
+  errorMessaagePassword: string = '';
+
+  onButtonClick() {
+
+    if (!this.email.includes('@')) {
+
+      this.errorMessage = 'Invalid Email, please enter a valid Email Address.';
+      return;
+    }
+    
+    this.errorMessage = '';
+
+    if(!this.password) {
+
+      this.errorMessaagePassword = 'Invalid Password, please enter a valid Password.'
+      return;
+    }
+
+    this.errorMessaagePassword = '';
+
+
+  }
+
+
+
 }
